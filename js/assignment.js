839e9594ffd0aea153ba8035dfee03c11b481b69
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2025-01-17 17:33:04 -0500

    find smallest and largest numbers

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,31 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+
+function findSmallestNumber(myNumbers){
+    let minNum = myNumbers[0]
+    for(let i = 1; i < myNumbers.length; i++){
+        if(myNumbers[i] <= minNum){
+            minNum = myNumbers[i]
+        }
+    }
+    return minNum
+    
+}
+// let test1 = findSmallestNumber(myNumbers)
+// console.log(test1);
+
+function findLargestNumber(arr){
+    let largest = arr[0]
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > largest){
+            largest = arr[i]
+        }
+    }
+    return largest
+    
+}
+
+// let test2 = findLargestNumber(myNumbers)
+// console.log(test2);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// fruits.forEach(function(fruit, index) {
+//     console.log(index + ': ' + fruit);
+// });
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(function(number) {
+//     console.log(number);
+// });
+
+// let protein = ["eggs","chicken","beef"]
+
+// for(let i = 0; i < protein.length; i++){
+//     console.log(protein[i]);
+// }
+
+// let array = [1,4,5,3,6,7]
+
+// function smallest(array){
+//     let smallNum = array[0]
+//     for(let i = 1; i < array.length; i++){
+//         if(array[i] < smallNum){
+//            smallNum =  array[i]
+//         }
+//     }
+// }
+// let test1 = smallest()
+// console.log(test1);
